c04f08fbf53b9e4b7c68a70029be2dec7bf6e65c
m-silvera0 <migueltenato@gmail.com>
2023-04-04 14:31:49 +0000

    isso ai

---
+++ Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente {
+    public static void main(String[] args){
+        Conta conta=new Conta();
+
+        conta.nome="Miguel";
+        conta.salario=1000000;
+    
+    System.out.println(conta.nome);
+    System.out.println(conta.salario);
+    }
+    
+}
---
+++ Conta.java
@@ -0,0 +1,26 @@
+public class Conta{
+    private int numero;
+    private double salario;
+    private String nome;
+    
+    
+    
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    public int getNumero(){
+        return this.numero;
+    }
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+    public double getSalario(){
+        return this.salario;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+}
